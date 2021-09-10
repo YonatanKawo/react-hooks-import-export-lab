a7e5145dbc4671c912ed67f32b97319d0916b21b
Yonatan Kawo <yonas478@gmail.com>
2021-09-09 22:55:46 -0500

    commit tree e7dea751bb4414e607f0a87c528a9d6214b2106f

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About  from "./components/About";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
